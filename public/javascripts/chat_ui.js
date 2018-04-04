83b2af25eb16d102f17fca4e0b7043ac4b3f0cb9
Mike <mcaruso@alum.mit.edu>
2018-04-04 18:02:48 -0400

    Graph display from Vision (YEA !!!)

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -16,26 +16,33 @@ function processUserInput(chatApp, socket) {
 var socket = io.connect();
 
 $(document).ready(function() {
-  var chatApp = new Chat(socket);
+    var chatApp = new Chat(socket);
 
-  socket.on('message', function (message) {
-      var messageText = message.text;
-      var newElement = messageText.substring (0,1) === "<"
-          ? divSystemContentElement (messageText)
-          : divEscapedContentElement (messageText);
-    $('#messages').append(newElement);
-    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
-  });
+    socket.on(
+	'message', function (message) {
+	    var messageText = message.text;
+	    var newElement = messageText.substring (0,1) === "<"
+		? divSystemContentElement (messageText)
+		: divEscapedContentElement (messageText);
+	    $('#messages').append(newElement);
+	    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+	}
+    );
+    socket.on(
+	'showGraph', function (source) {
+	    console.log ("Got showGraph " + source);
+	    showGraph (source);
+	}
+    );
 
-  socket.on('ping-pong', function (message) {
-  });
+    socket.on('ping-pong', function (message) {});
 
 //  setInterval (function() {socket.emit ('ping-pong');}, 750);
 
-  $('#send-message').focus();
+    $('#send-message').focus();
 
-  $('#send-form').submit(function() {
-    processUserInput(chatApp, socket);
-    return false;
-  });
+    $('#send-form').submit(function() {
+	processUserInput(chatApp, socket);
+	return false;
+    });
 });
