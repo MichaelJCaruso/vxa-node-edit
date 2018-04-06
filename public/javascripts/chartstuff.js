f44dc9a01574c10e315cfe225b813e4133318184
Mike <mcaruso@alum.mit.edu>
2018-04-06 01:40:38 -0400

    Make scrollable output and resizing work

--- public/javascripts/chartstuff.js
+++ public/javascripts/chartstuff.js
@@ -1,23 +1,29 @@
 //     d3.json("./tweets.json", data => histogram(data.tweets) );
 
+var whichgram = histogram2;
 function showGraph (url) {
-    d3.json(url, data => histogram(data.tweets) );
+    $('#viz-svg').empty ();
+    d3.json(
+	url,
+	data => whichgram(data.tweets, d => d.favorites.length)
+    );
 }
 
-function histogram(tweetsData) {
+/********************************/
+function histogram1(input,accessor) {
     var xScale = d3.scaleLinear().domain([ 0, 5 ]).range([ 0, 500 ]);
     var yScale = d3.scaleLinear().domain([ 0, 10 ]).range([ 400, 0 ]);
     var xAxis = d3.axisBottom().scale(xScale).ticks(5);
-    
+
     var histoChart = d3.histogram();
-    
+
     histoChart
         .domain([ 0, 5 ])
         .thresholds([ 0, 1, 2, 3, 4, 5 ])
-        .value(d => d.favorites.length);
-    
-    histoData = histoChart(tweetsData);
-    
+        .value(accessor);
+
+    histoData = histoChart(input);
+
     d3.select("svg")
         .selectAll("rect")
         .data(histoData).enter()
@@ -32,4 +38,61 @@ function histogram(tweetsData) {
         .attr("transform", "translate(0,400)").call(xAxis);
     
     d3.select("g.axis").selectAll("text").attr("dx", 50);
+}
+
+/********************************/
+function histogram2(input,accessor) {
+    var formatCount = d3.format(",.0f");
+
+    var minWidth  = 2,
+	minHeight = 2;
+    
+    var svg = d3.select("svg"),
+	margin = {top: 10, right: 30, bottom: 30, left: 30},
+	width = Math.max(+svg.attr("width") - margin.left - margin.right,minWidth),
+	height = Math.max(+svg.attr("height") - margin.top - margin.bottom,minHeight),
+	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+/*
+    var x = d3.scaleLinear()
+	.rangeRound([0, width]);
+*/
+    var x = d3.scaleLinear().domain([ 0, 5 ]).range([ 0, 500 ]);
+
+    var bins = d3.histogram()
+	.domain(x.domain())
+    	.thresholds(x.ticks(20))
+	.value(accessor)
+    (input);
+    console.log (bins);
+
+    var y = d3.scaleLinear().domain([ 0, 10 ]).range([ 400, 0 ]);
+/*
+    var y = d3.scaleLinear()
+	.domain([0, d3.max(bins, function(d) { return d.length; })])
+	.range([height, 0]);
+*/
+
+    var bar = g.selectAll(".bar")
+	.data(bins)
+	.enter().append("g")
+	.attr("class", "bar")
+	.attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+    bar.append("rect")
+	.attr("x", 1)
+	.attr("width", Math.max(x(bins[0].x1) - x(bins[0].x0) - 1, minWidth))
+        .attr("height", function(d) { return Math.max(height - y(d.length), minHeight); });
+
+    bar.append("text")
+	.attr("dy", ".75em")
+	.attr("y", 6)
+	.attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+	.attr("text-anchor", "middle")
+	.text(function(d) { return formatCount(d.length); });
+
+    g.append("g")
+	.attr("class", "axis axis--x")
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x));
 }
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -13,6 +13,22 @@ function processUserInput(chatApp, socket) {
   $('#send-message').val('');
 }
 
+function onTextAreaResize (area, handler) {
+    area.on("mouseup", handler)
+};
+
+function updateMessageAreaHeight () {
+    $('#message-area').height(
+	Math.max (
+	    $(window).height()
+		- $('#send-form-area').height()
+		- $('#viz').height()
+		- 20,
+	    100
+	)
+    );
+}
+
 var socket = io.connect();
 
 $(document).ready(function() {
@@ -25,7 +41,7 @@ $(document).ready(function() {
 		? divSystemContentElement (messageText)
 		: divEscapedContentElement (messageText);
 	    $('#messages').append(newElement);
-	    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+//	    $('#messages').scrollTop($('#messages').prop('height'));
 	}
     );
     socket.on(
@@ -39,7 +55,13 @@ $(document).ready(function() {
 
 //  setInterval (function() {socket.emit ('ping-pong');}, 750);
 
-    $('#send-message').focus();
+    updateMessageAreaHeight ();
+    $(window).resize (updateMessageAreaHeight);
+
+    (area => {
+	onTextAreaResize (area, updateMessageAreaHeight);
+	area.focus ();
+    }) ($('#send-message'));
 
     $('#send-form').submit(function() {
 	processUserInput(chatApp, socket);
