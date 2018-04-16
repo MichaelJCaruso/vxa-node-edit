2358530d5bbdcad62d99bd0f401c192c1d3cb95d
Mike <mcaruso@alum.mit.edu>
2018-04-16 11:47:21 -0400

    Add logging

--- server.js
+++ server.js
@@ -1,28 +1,54 @@
 const express = require('express');
+const fs = require('fs');
 const http = require('http');
+const morgan = require('morgan');
 const path = require('path');
 
-/*----------------*/
+/*-------------------*/
+/*----  express  ----*/
+/*-------------------*/
 const app = express ();
-app.use (express.static (path.join (__dirname, 'public')));
-app.use ('/node_modules', express.static (path.join (__dirname, 'node_modules')));
 module.exports.app = app;
 
-/*----------------*/
+/*-----------------*/
+/*---- logging ----*/
+/*-----------------*/
+app.use (
+    morgan (
+        'tiny', {
+            stream: fs.createWriteStream(
+                path.join(__dirname, '/logs', 'access.log'), {flags: 'a'}
+            )
+        }
+    )
+);
+
+/*--------------------------*/
+/*----  static content  ----*/
+/*--------------------------*/
+app.use (express.static (path.join (__dirname, 'public')));
+app.use ('/node_modules', express.static (path.join (__dirname, 'node_modules')));
+
+/*-----------------------*/
+/*----  http server  ----*/
+/*-----------------------*/
 const httpServer = http.createServer (app);
 module.exports.httpServer = httpServer;
 
+/*--------------------------*/
+/*----  the good stuff  ----*/
+/*--------------------------*/
+
 /*----------------*/
 const visionServer = require('./lib/vision_server');
-app.use ('/cgi-bin/vquery.exe', visionServer.VQueryHandler ());
-
 module.exports.visionServer = visionServer;
 module.exports.v = visionServer.v;
+app.use ('/cgi-bin/vquery.exe', visionServer.VQueryHandler ());
 
 /*----------------*/
 const chatServer = require('./lib/chat_server');
-chatServer.listen(httpServer);
 module.exports.chatServer = chatServer;
+chatServer.listen(httpServer);
 
 /*----------------*/
 httpServer.listen(process.env.PORT || 3000, function() {
