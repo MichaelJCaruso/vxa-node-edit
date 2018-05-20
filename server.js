4d085a7b0c510aba5474d09477f13ec3522770ac
Mike <mcaruso@alum.mit.edu>
2018-05-20 16:19:59 -0400

    Deploy https: server and new api conventions
    Both old and new forms are supported, but here's what's preferred:
    
       New                              Old
       ===                              ===
       http://.../vision/...            http://.../cgi-bin/vquery.exe/...
       https://.../vision/api/...       http://.../cgi-bin/vquery.exe/bare/...

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const http = require('http');
+const https = require('https');
 const morgan = require('morgan');
 const path = require('path');
 
@@ -9,6 +12,8 @@ const path = require('path');
 /*-------------------*/
 const app = express ();
 module.exports.app = app;
+app.use (cors());
+app.use (bodyParser.json());
 
 /*-----------------*/
 /*---- logging ----*/
@@ -29,11 +34,18 @@ app.use (
 app.use (express.static (path.join (__dirname, 'public')));
 app.use ('/node_modules', express.static (path.join (__dirname, 'node_modules')));
 
+/*-------------------*/
+/*----  servers  ----*/
+/*-------------------*/
+const httpServer = http.createServer (app);
+
 /*-----------------------*/
 /*----  http server  ----*/
 /*-----------------------*/
-const httpServer = http.createServer (app);
-module.exports.httpServer = httpServer;
+const httpsServer = https.createServer ({
+    key : fs.readFileSync('./config/certs/key.pem'),
+    cert: fs.readFileSync('./config/certs/cert.pem')
+}, app);
 
 /*--------------------------*/
 /*----  the good stuff  ----*/
@@ -42,8 +54,8 @@ module.exports.httpServer = httpServer;
 /*----------------*/
 const visionServer = require('./lib/vision_server');
 module.exports.visionServer = visionServer;
-module.exports.v = visionServer.v;
-app.use ('/cgi-bin/vquery.exe', visionServer.vquery);
+app.use ('/cgi-bin/vquery.exe', visionServer.app);
+app.use ('/vision', visionServer.app);
 
 /*----------------*/
 const vdashServer = require('./lib/vdash_server');
@@ -51,10 +63,23 @@ module.exports.vdashServer = vdashServer;
 vdashServer.listen(httpServer);
 
 /*----------------*/
-var port = process.env.PORT || 3000;
-httpServer.listen(port, function() {
-  console.log("Server listening on port " + port);
-});
+const ports = (port=>({
+    http : +port,
+    https: +port+443
+}))(process.env.PORT || 3000);
+module.exports.ports = ports;
+
+const servers = {
+    http : httpServer,
+    https: httpsServer
+};
+module.exports.servers = servers;
+
+['http','https'].map(
+    k=>((k,s,p)=>s.listen(p,()=>{
+        console.log(`'${k}' server listening on port ${p}`);
+    }))(k,servers[k],ports[k])
+);
 
 /*
 FormatTools Html evaluate: [Interface ApplicationWS ShowClassSummary];
